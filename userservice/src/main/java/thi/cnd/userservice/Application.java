30120d3e142bd2d1deb793a9a7fd89e1a4a008aa
ca-schue <carl.schuenemann@gmail.com>
2023-11-13 19:42:10 +0100

    Refactored User Service (Tested)

--- userservice/src/main/java/thi/cnd/userservice/Application.java
+++ userservice/src/main/java/thi/cnd/userservice/Application.java
@@ -2,10 +2,12 @@ package thi.cnd.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 //import thi.cnd.userservice.secondary.event.config.KafkaTopicConfig;
 
 //@EnableConfigurationProperties(KafkaTopicConfig.class)
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
